5226d03ec01560d63b53041466b4e872790eea21
Matthew Zenn <spyguy57@gmail.com>
2022-11-08 15:59:39 -0500

    added convert function

--- logic.js
+++ logic.js
@@ -1,3 +1,5 @@
+var tweet = document.getElementById("tweet");
+
 document.getElementById("copy").addEventListener("click", function() {
     var copytext = document.getElementById("tweet");
     navigator.clipboard.writeText(copytext.value);
@@ -12,4 +14,27 @@ document.getElementById("mockingjay").addEventListener("click", function() {
     let wins = document.querySelector(".window");
     wins.style.display = "none";
     document.getElementById("mock").style.display = "block";
+});
+
+document.getElementById("regex").addEventListener('click', function () {
+    var re = tweet.value;
+    var res = re.replace(/oe/gi, 'œ');
+    re = res;
+    res = re.replace(/oo/gi, 'ꚙ');
+    re = res;
+    res = re.replace(/ae/gi, 'æ');
+    re = res;
+    res = re.replace(/ij/gi, 'ĳ');
+    re = res;
+    res = re.replace(/hu/gi, 'ƕ');
+    re = res;
+    res = re.replace(/dz/gi, 'ǳ');
+    re = res;
+    res = re.replace(/nj/gi, 'ǌ');
+    re = res;
+    res = re.replace(/bl/gi, 'Ы');
+    re = res;
+    res = re.replace(/oy/gi, 'ѹ');
+
+    tweet.value = res;
 });
